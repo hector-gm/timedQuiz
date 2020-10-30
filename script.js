72dbb1771c4cffc8a4c81cf4f18fc49b21169b99
Hector Garcia Millares <hgarcia.millares@gmail.com>
2020-10-29 22:28:32 -0400

    Going over .js and annotating as possible scope error is preventing .html display of questions

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-// Define our initial score and time at the start of the quiz
+// Define our initial score and time at the start of the quiz   A.K.A. declaring global variables for the other functions
 var score = 0;
 var timeRemaining = 0;
-var timer;
+var timer; // leaving this without a value because it will be defined by a function to be dynamic
 var currentQuestion= -1; // Set to -1 since questions are in an array and we want to display the question at index 0 too
 
-// Define the questions, multiple choice options and correct answers for our quiz
+// Define the questions, multiple choice options and correct answers for our quiz.  These are also meant to be Global and accesible to all functions
 var questions = [ 
     {
     ask:"A very useful tool used during development and debuggin for printing content to the debugger is:",
@@ -37,52 +37,39 @@ var questions = [
     answer:"Alerts"
     }
 ]
-$("<button>").click()
+
+console.log("Number of questions in this quiz:" + questions.length);
+
 
 // Start the countdown clock once quiz begins
 function startQuiz() {
     timeRemaining=questions.length*15; // Set max Timer
    document.getElementById("countdown").innerHTML=timeRemaining; //display countdown in html
    
-   timer=setInterval(function(){ // set function on an interval to repeat
+   timer = setInterval(function(){   // set function on an interval to repeat. This is what sets the value for the declared global variable
        timeRemaining--; // set countdown to reduce 1 value at a time
-       document.getElementById("countdown").innerHTML=timeRemaining; //displays new countdown value in html
+       document.getElementById("countdown").innerHTML = timeRemaining; //displays new countdown value in html
        if (timeRemaining<=0) {  // set countdown value at which function will stop the quiz
-           clearInterval(timer);    // clear the repeat interval
+           clearInterval(timer);    // clear the repeat interval if the value were to be a negative interger
             endQuiz();          // end the quiz if the condition is met
         } //Close IF argument
-   },1000); // Close interval with repeat cycle set to 1000 milliseconds
+   },1000); // Repeat cycle set to 1000 milliseconds (1 second) while the if statement remains in effect
   
-   nextQuestion(); // Instruction to run the next question function
+   nextQuestion(); // Instruction to run the next question function and display its code as instructed below
+
+   console.log("Time alloted per question is:" + timeRemaining);
 }
 
 function endQuiz() {    // Ends the quiz
     clearInterval(timer);    //Clear the instruction to repeat the timer function
-    var quiz = '<h2>End of quiz!!!</h2> <input type="text" id="user" placeholder="Initials"> <button onclick="saveScore()">Submit your score to the scoreboard!</button>';
+    var displayContent = `
+    <h2>End of quiz!!!</h2> 
+    <input type="text" id="user" placeholder="Initials">
+    <button onclick="saveScore()">Submit your score to the scoreboard!</button>`;
 
-    document.querySelector("container").innerHTML=quiz;
+    document.querySelector("container").innerHTML = displayContent; // Instruction to replace the container's content with the function's displayMessage variable
 }
-function nextQuestion() {
-    currentQuestion++;  // Increase the index of the current question by value of 1 every time the function is called
 
-    if (currentQuestion>questions.length) { // Set condition to stop the quiz once all questions have been presented
-        endQuiz(); // Run the function to end quiz
-        return; //
-    }
-    var quiz= "<h2>" + questions[currentQuestion].ask + "</h2>" // Current question text to be displayed from "ask" property
-    console.log(quiz);
-    for (var runQuestions=0;runQuestions<questions[currentQuestion].choices; runQuestions++) {
-        var showChoices="<button onclick=\"[ANS]\">[OPTION]</button>";
-        showChoices=showChoices.replace("[OPTION]", questions[currentQuestion].choices[runQuestions]);
-        if (questions[currentQuestion].choices[runQuestions]==questions[currentQuestion].answer){
-            showChoices=showChoices.replace("[ANS]", correctAnswer());
-        } else {
-            showChoices=showChoices.replace("[ANS]", wrongAnswer());
-        }
-        quiz += runQuestions
-    }
-    document.querySelector("container").innerHTML=quiz;
-}
 
 function saveScore() {
     localStorage.setItem("highscore", score);
@@ -91,8 +78,8 @@ function saveScore() {
 }
 
 function getScore() {
-    var quiz = "<h2>" + localStorage.getItem("highscore")+"</h2>";
-    document.querySelector("container").innerHTML=quiz;
+    var displayContent = "<h2>" + localStorage.getItem("highscore")+"</h2>";
+    document.querySelector("container").innerHTML= displayContent;
 }
 
 function wrongAnswer() {
@@ -104,7 +91,29 @@ function correctAnswer () {
     nextQuestion();
 }
 
+function nextQuestion() {
+    currentQuestion++;  // Increase the index of the current question by value of 1 every time the function is called
 
+    if (currentQuestion > questions.length - 1) { // Set condition to stop the quiz once all questions have been presented
+        endQuiz(); // Run the function to end quiz
+        return; 
+    }
+
+    var displayContent= "<h2>" + questions[currentQuestion].ask + "</h2>" // Current question text to be displayed from "ask" property
+    console.log(displayContent) // Logging function up to this point as something isn't working properly
+    
+    for (var runQuestions = 0;runQuestions < questions[currentQuestion].choices.length; runQuestions++) {
+        var showChoices="<button onclick=\"[ANS]\">[OPTION]</button>";
+        showChoices=showChoices.replace("[OPTION]", questions[currentQuestion].choices[runQuestions]);
+        if (questions[currentQuestion].choices[runQuestions]==questions[currentQuestion].answer){
+            showChoices=showChoices.replace("[ANS]", correctAnswer());
+        } else {
+            showChoices=showChoices.replace("[ANS]", wrongAnswer());
+        }
+        displayContent += runQuestions
+    }
+    document.querySelector("container").innerHTML= displayContent;
+}
 
 
 
